3297e793ec40fb1126359a3b650992c1814e8b05
unknown <thebeangodzilla22@gmail.com>
2024-03-16 23:23:48 +0800

    2nd commit, styled the menu bar

--- src/components/navbar/menu.jsx
+++ src/components/navbar/menu.jsx
@@ -4,7 +4,7 @@ import menu from './menu.css'
 export default function Menu(props) {
     return (
         <div className={`menu  ${props.state ? 'expanded' : ''}`}>
-            <div className='flex justify-center'>
+            <div className='flex pt-4 menu-item justify-center w-100 pb-4'>
                 <a href='#' className='text-2xl'>Contact</a>
             </div>
         </div>
