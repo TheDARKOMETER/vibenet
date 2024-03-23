83c85543dd29929f65bccdeb7c0f9ff912440267
unknown <thebeangodzilla22@gmail.com>
2024-03-23 21:43:30 +0800

    Used tailwind react datepicker from npm

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,64 @@
 import './landing.css'
 import { useState } from 'react';
-import DatePicker from "react-datepicker";
+import Datepicker from "tailwind-datepicker-react"
+
+
 
 export default function Landing() {
-    const [birthday, setBirthday] = useState(new Date())
+    const [show, setShow] = useState(false)
+
+    // Code provided by tailwind-datepicker-react readme
+    // TODO: Provide light/dark theme context
+
+    const options = {
+        title: "Birthdate",
+        autoHide: true,
+        todayBtn: false,
+        clearBtn: true,
+        clearBtnText: "Clear",
+        maxDate: new Date("2030-01-01"),
+        minDate: new Date("1950-01-01"),
+        theme: {
+            background: "bg-gray-700 dark:bg-gray-800",
+            todayBtn: "",
+            clearBtn: "",
+            icons: "",
+            text: "",
+            disabledText: "bg-red-500",
+            input: "",
+            inputIcon: "",
+            selected: "",
+        },
+        icons: {
+            // () => ReactElement | JSX.Element
+            prev: () => <span>Previous</span>,
+            next: () => <span>Next</span>,
+        },
+        datepickerClassNames: "top-12",
+        defaultDate: new Date("2022-01-01"),
+        language: "en",
+        disabledDates: [],
+        weekDays: ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+        inputNameProp: "date",
+        inputIdProp: "date",
+        inputPlaceholderProp: "Select Date",
+        inputDateFormatProp: {
+            day: "numeric",
+            month: "long",
+            year: "numeric"
+        }
+    }
+
+
+    const handleChange = (selectedDate) => {
+        console.log(selectedDate)
+    }
+    const handleClose = (state) => {
+        setShow(state)
+    }
 
     return (
-        <div className='landing-container page-container flex  mr-4 ml-4 center-margin'>
+        <div className='landing-container page-container flex mr-4 ml-4 center-margin'>
             <div className='hero-heading'>
                 <h1>
                     Connect, Vibe, and Discover: Your Social Media Journey Starts Here!
@@ -17,11 +69,15 @@ export default function Landing() {
             </div>
             <div className='signup-form flex flex-col justify-center items-center'>
                 <h1>Let’s get started!</h1>
-                <input type='text' id='username' placeholder='Username' />
-                <input type='text' id='password' placeholder='Password' />
-                <input type='text' id='confpass' placeholder='Confirm Password' />
-                <input type='text' id='email' placeholder='Email' />
-                <DatePicker selected={birthday} onChange={(date) => setBirthday(date)} />
+                <input className='landing-form' type='text' id='username' placeholder='Username' />
+                <input className='landing-form' type='text' id='password' placeholder='Password' />
+                <input className='landing-form' type='text' id='confpass' placeholder='Confirm Password' />
+                <input className='landing-form' type='text' id='email' placeholder='Email' />
+                <div className='date-picker-wrapper flex flex-row items-center w-80'>
+                    <span>Birthdate: </span>
+                    <Datepicker options={options} onChange={handleChange} show={show} setShow={handleClose} />
+                    <input className='landing-form' type='text' id='gender'></input>
+                </div>
             </div>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./node_modules/tailwind-datepicker-react/dist/**/*.js"],
   theme: {
     screens: {
       'sm': '768px'
